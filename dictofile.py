42ad99bb561af7221b7dbca55da2abe41a38af53
meriemfed <154727592+meriemfed@users.noreply.github.com>
2024-03-24 00:38:51 +0100

    Update dictofile.py

--- dictofile.py
+++ dictofile.py
@@ -1,16 +1,17 @@
 import pickle
 import hashlib
 
+#this code turns a txt file into a dictionnary {key:hash,value:password}
 password_dict={}
+#init a dict
 
-
-with open('liste.txt', 'r') as file:
+with open("liste.txt", 'r') as file:
     
     for line in file:
         
         value=line.strip()
         key=hashlib.md5(value.encode()).hexdigest()
         password_dict[key]=value
-    
+#save the dictionnary as a pickle file(byte stream)
     with open('password_dict.pkl', 'wb') as fp:
         pickle.dump(password_dict, fp)
