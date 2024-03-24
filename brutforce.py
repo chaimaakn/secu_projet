321d144e05f35a109ee838c71681f81aaa53d3c5
Haniaa567 <154731128+Haniaa567@users.noreply.github.com>
2024-03-24 23:03:44 +0100

    Update brutforce.py

--- brutforce.py
+++ brutforce.py
@@ -2,6 +2,7 @@ import itertools
 import string
 import time
 import hashlib
+import sys
 
 # Définir les caractères autorisés
 CARACTERES = string.ascii_letters + string.digits + string.punctuation
@@ -11,7 +12,7 @@ def md5(mot):
     return hashlib.md5(mot.encode()).hexdigest()
 
 # Hachage MD5 du mot à trouver
-HASH_MOT_A_TROUVER = '5f3f4681121b460e3304a1887f42f1c3'   #bat
+HASH_MOT_A_TROUVER = '5a105e8b9d40e1329780d62ea2265d8a'  # bat
 
 # Fonction pour tester si un mot correspond au hachage
 def est_bon_mot(mot):
@@ -20,21 +21,24 @@ def est_bon_mot(mot):
 # Fonction pour trouver le bon mot
 def trouver_bon_mot():
     longueur = 1
-    start_time = time.time() # Enregistrer le temps de départ
+    start_time = time.time()  # Enregistrer le temps de départ
+    mots_testes = 0
     while True:
-        for prefixe in itertools.product(CARACTERES, repeat=longueur-1):
-            for dernierCaractere in CARACTERES:
-                mot = ''.join(prefixe) + dernierCaractere
-                if est_bon_mot(mot):
-                    end_time = time.time() # Enregistrer le temps de fin
-                    temps_ecoule = end_time - start_time
-                    return mot, temps_ecoule
+        for mot in (''.join(carac) for carac in itertools.product(CARACTERES, repeat=longueur)):
+            mots_testes += 1
+            if mots_testes % 10000 == 0:
+                print(f"Mot testé : {mot}")
+                sys.stdout.flush()  # Forcer l'affichage immédiat du message
+            if est_bon_mot(mot):
+                end_time = time.time()  # Enregistrer le temps de fin
+                temps_ecoule = end_time - start_time
+                return mot, temps_ecoule
         longueur += 1
 
 # Exemple d'utilisation
 bon_mot, temps_ecoule = trouver_bon_mot()
 if bon_mot:
-    print(f"Le mot correspondant au hachage {HASH_MOT_A_TROUVER} est: {bon_mot}")
+    print(f"\nLe mot correspondant au hachage {HASH_MOT_A_TROUVER} est: {bon_mot}")
     print(f"Temps écoulé pour trouver le mot: {temps_ecoule:.6f} secondes")
 else:
     print("Aucun mot n'a été trouvé.")
