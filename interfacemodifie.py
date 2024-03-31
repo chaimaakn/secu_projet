a07e415061235eb1db5de97da81c611a2fa6e9db
Meriem Fed <meriemfedaoui1@gmail.com>
2024-03-31 16:24:12 +0100

    little addition

--- interfacemodifie.py
+++ interfacemodifie.py
@@ -150,6 +150,8 @@ def hide_all_frames():
     result_frame_lookup_table.place_forget()
     result_label_lookup_table.place_forget()
     password_label_lookup_table.place_forget()
+    label_lookup_table.place_forget()
+    entry_lookup_table.place_forget()
     
 
 
