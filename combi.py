62747e3f52cc7490a61fe7bb697a309afaba62a2
meriemhcn <meriem200414@gmail.com>
2024-05-02 11:05:49 +0100

    combi

--- combi.py
+++ combi.py
@@ -1,16 +1,16 @@
 from passlib.hash import md5_crypt
 from passlib.hash import sha1_crypt
-'''
+'''r=10000
 def crack_password(hash_value, dictionary_file):
     with open(dictionary_file, 'r') as f:
         for line in f:
             password = line.strip()
-            if md5_crypt.verify(password, hash_value):
+            if sha1_crypt.using(rounds=r).verify(password, hash_value):
                 return password
     return None
 
 # Exemple d'utilisation 
-hash_value = "$1$zh7k6TPq$vSqNsHYIhb0HGuQMjAnp6/"  # Exemple de hachage MD5 avec sel inclus
+hash_value = "$sha1${r}$mNJ22a0S$.eZu8s6oiSoDsf..fxcf3KG2ebPY"  # Exemple de hachage MD5 avec sel inclus
 dictionary_file = "liste.txt"  # Fichier de dictionnaire
 
 password = crack_password(hash_value, dictionary_file)
