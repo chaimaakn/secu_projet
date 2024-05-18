b6d727ea3014d303bc480027cb545506ad1a24ff
Haniaa567 <154731128+Haniaa567@users.noreply.github.com>
2024-05-18 19:38:26 +0100

    sha256

---
+++ lookupTablefinale.py
@@ -0,0 +1,18 @@
+import pickle
+import hashlib
+
+#this code turns a txt file into a dictionnary {key:hash,value:password}
+password_dict={}
+#init a dict
+
+with open("liste.txt", 'r') as file:
+    
+    for line in file:
+        
+        value=line.strip()
+        #key=hashlib.md5(value.encode()).hexdigest()   #pour créer la tqble de md5
+        key=hashlib.sha256(value.encode()).hexdigest()   #pour créer la tqble de sha1
+        password_dict[key]=value
+#save the dictionnary as a pickle file(byte stream)
+    with open('password_dict_sha256.pkl', 'wb') as fp:
+        pickle.dump(password_dict, fp)
