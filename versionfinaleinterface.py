2638e25633ac1bb86f9ca3f48027071c4e772829
Souha Aknouche <aknouchechaima00@gmail.com>
2024-05-22 13:33:47 +0100

    version avec salt

--- versionfinaleinterface.py
+++ versionfinaleinterface.py
@@ -562,9 +562,10 @@ def est_bon_mot(mot, hash_a_trouver):
     
     
 
+    
 password_found = False
 password_lock = threading.Lock()
-le_bon_MotDePasse = None       
+le_bon_MotDePasse = None  
 # Function to generate passwords
 def generate_passwords(characters, length):
     for combination in itertools.product(characters, repeat=length):
@@ -582,26 +583,38 @@ def thread_function(password_hash, characters, length, hash_function):
         print(f"Testing password: {password}") 
 
         password_bytes = password.encode('utf-8')
-        generated_hash = hash_function(password_bytes)
-
+        
+        generated_hash =hash_function(password_bytes)
         if generated_hash == password_hash:
-            with password_lock:
-                if not password_found:
+              with password_lock:
+                 if not password_found:
                     password_found = True
                     le_bon_MotDePasse = password
-            return
+                    return
 
 # Hash function selector based on button click
-def get_hash_function(dernier_bouton_clique):
-    if dernier_bouton_clique == 1:
-        return lambda password_bytes: hashlib.md5(password_bytes).hexdigest()
-    elif dernier_bouton_clique == 2:
-        return lambda password_bytes: hashlib.sha1(password_bytes).hexdigest()
+def get_hash_function(dernier_bouton_clique,var2,salt_hash):
+    
+    
+    if var2.get()==1:
+        if dernier_bouton_clique==1:
+             return lambda password_bytes: md5_crypt.using(salt=salt_hash).hash(password_bytes)
+        elif dernier_bouton_clique==2:
+          return lambda password_bytes: sha1_crypt.using(salt=salt_hash).hash(password_bytes)
+        else:
+           return lambda password_bytes: sha256_crypt.using(salt=salt_hash,round=1000).hash(password_bytes) 
     else:
-        return lambda password_bytes: hashlib.sha256(password_bytes).hexdigest()
+        if dernier_bouton_clique==1:
+           return lambda password_bytes: hashlib.md5(password_bytes).hexdigest()
+        elif dernier_bouton_clique==2:
+            return lambda password_bytes: hashlib.sha1(password_bytes).hexdigest()
+        else:
+            return lambda password_bytes: hashlib.sha256(password_bytes).hexdigest()
+
+
 
 # Main brute force function
-def brute_force_password(password_hash, dernier_bouton_clique, max_length=12, num_threads=None):
+def brute_force_password(password_hash, dernier_bouton_clique, var2,salt_hash,max_length=12, num_threads=None):
     global password_found
     global le_bon_MotDePasse
 
@@ -610,7 +623,7 @@ def brute_force_password(password_hash, dernier_bouton_clique, max_length=12, nu
     le_bon_MotDePasse = None
 
     characters = string.ascii_letters + string.digits + string.punctuation
-    hash_function = get_hash_function(dernier_bouton_clique)
+    hash_function = get_hash_function(dernier_bouton_clique,var2,salt_hash)
 
     if num_threads is None:
         num_threads = min(24, os.cpu_count() * 2)
@@ -631,6 +644,7 @@ def retrouver_mot():
     global dernier_bouton_clique
 
     hash_input = entry_brut_force.get().strip()
+    salt_hash=None
     if var2.get() == 1:
         salt_hash = entry_salt2.get().strip()
         if messagebox_salt(salt_hash):
@@ -658,7 +672,7 @@ def retrouver_mot():
             if message_box_sha256(hash_input):
                 return
 
-    le_bon_MotDePasse = brute_force_password(hash_input, dernier_bouton_clique)  # Call the brute_force_password function with the provided hash and method
+    le_bon_MotDePasse = brute_force_password(hash_input, dernier_bouton_clique,var2,salt_hash)  # Call the brute_force_password function with the provided hash and method
 
     if le_bon_MotDePasse:
         retry_button_brute_force.pack(side=tk.LEFT, padx=10)
@@ -666,6 +680,9 @@ def retrouver_mot():
         toggle_back_button(False)
     else:
         messagebox.showinfo("Information", "Aucun mot n'a été trouvé.")
+
+        
+        
 def show_brute_force_interface():
     global entry_brut_force 
     global current_frame
